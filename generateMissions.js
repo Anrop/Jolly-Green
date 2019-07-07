1d0ad8918425efdddcf5c4e2aea78ca431beff34
Björn Dahlgren <bjorn@dahlgren.at>
2019-07-07 15:24:40 +0200

    Generate mission per mod

--- generateMissions.js
+++ generateMissions.js
@@ -1,28 +1,112 @@
 const fs = require('fs-extra')
 const path = require('path')
 
+const mods = require('./mods')
 const worlds = require('./worlds')
 
 const outputDirectory = 'build'
 const baseMissionDirectory = 'mission'
 const worldsDirectory = 'worlds'
 
+function replaceUnits (content, units) {
+  if (!units) {
+    return content
+  }
+
+  for (const originalClass in units) {
+    const replacementClass = units[originalClass]
+    content = content.replace(new RegExp(originalClass, 'g'), replacementClass)
+  }
+  return content
+}
+
+function updateDescriptionFile (descriptionFile, mod) {
+  return readFile(descriptionFile)
+    .then((content) => content.replace('CSAR Jolly Green 28', `CSAR Jolly Green 28 (${mod.name})`))
+    .then((content) => content.replace('CSAR JOLLY GREEN 28', `CSAR JOLLY GREEN 28 (${mod.name})`))
+    .then((content) => writeFile(descriptionFile, content))
+}
+
+function updateInitServerFile (initServerFile, mod, type) {
+  const units = mod[type] || mod.woodland || {}
+
+  return readFile(initServerFile)
+    .then((content) => replaceUnits(content, units.redfor))
+    .then((content) => writeFile(initServerFile, content))
+}
+
+function updateMissionFile (missionFile, mod, type) {
+  const units = mod[type] || mod.woodland || {}
+
+  return readFile(missionFile)
+    .then((content) => content.replace('CO 11 CSAR Jolly Green 28', `CO 11 CSAR Jolly Green 28 (${mod.name})`))
+    .then((content) => replaceUnits(content, units.blufor))
+    .then((content) => writeFile(missionFile, content))
+}
+
+function updateParaTroopsFile (paraTroopsFile, mod, type) {
+  const units = mod[type] || mod.woodland || {}
+
+  return readFile(paraTroopsFile)
+    .then((content) => replaceUnits(content, units.redfor))
+    .then((content) => writeFile(paraTroopsFile, content))
+}
+
+function updateMission (missionDirectory, mod, type) {
+  const descriptionFile = path.join(missionDirectory, 'description.ext')
+  const initServerFile = path.join(missionDirectory, 'init_server.sqf')
+  const missionFile = path.join(missionDirectory, 'mission.sqm')
+  const paraTroopsFile = path.join(missionDirectory, 'paraTroops.sqf')
+
+  return Promise.all([
+    updateDescriptionFile(descriptionFile, mod),
+    updateInitServerFile(initServerFile, mod, type),
+    updateMissionFile(missionFile, mod, type),
+    updateParaTroopsFile(paraTroopsFile, mod, type)
+  ])
+}
+
 function createWorldVersions (world) {
-  const { name, mission } = world
+  mods.map((mod) => {
+    const missionWorldDirectory = path.join(worldsDirectory, world.mission)
+    const missionWorldFile = path.join(missionWorldDirectory, 'mission.sqm')
+    const missionOutputDirectory = path.join(outputDirectory, `${world.name}${mod.suffix}`)
+    const missionOutputFile = path.join(missionOutputDirectory, 'mission.sqm')
 
-  const missionWorldDirectory = path.join(worldsDirectory, mission)
-  const missionWorldFile = path.join(missionWorldDirectory, 'mission.sqm')
-  const missionWorldOutputDirectory = path.join(outputDirectory, name)
-  const missionWorldOutputFile = path.join(missionWorldOutputDirectory, 'mission.sqm')
-
-  return fs.emptyDir(missionWorldOutputDirectory)
-    .then(() => fs.copy(baseMissionDirectory, missionWorldOutputDirectory))
-    .then(() => fs.copy(missionWorldFile, missionWorldOutputFile))
+    return fs.emptyDir(missionOutputDirectory)
+      .then(() => fs.copy(baseMissionDirectory, missionOutputDirectory))
+      .then(() => fs.copy(missionWorldFile, missionOutputFile))
+      .then(() => updateMission(missionOutputDirectory, mod, world.type))
+  })
 }
 
 function createWorlds () {
   return fs.emptyDir(outputDirectory)
     .then(() => Promise.all(worlds.map(createWorldVersions)))
+}
+
+function readFile (file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, { encoding: 'utf8' }, (err, content) => {
+      if (err) {
+        return reject(err)
+      }
+
+      return resolve(content)
+    })
+  })
+}
+
+function writeFile (file, content) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, content, { encoding: 'utf8' }, (err) => {
+      if (err) {
+        return reject(err)
+      }
+
+      return resolve()
+    })
+  })
 }
 
 createWorlds()
---
+++ mods.js
@@ -0,0 +1,68 @@
+module.exports = [
+  {
+    name: 'Vanilla',
+    suffix: '_vanilla'
+  },
+  {
+    name: 'CUP',
+    suffix: '_cup',
+    woodland: {
+      blufor: {
+        B_Heli_Transport_01_camo_F: 'CUP_B_UH60S_USN',
+        B_helicrew_F: 'CUP_B_USMC_Pilot',
+        B_Helipilot_F: 'CUP_B_USMC_Pilot',
+        B_medic_F: 'CUP_B_USMC_Medic',
+        B_Pilot_F: 'CUP_B_USMC_Pilot',
+        B_soldier_AAT_F: 'CUP_B_USMC_Soldier_LAT',
+        B_soldier_AR_F: 'CUP_B_USMC_Soldier_AR',
+        B_soldier_AT_F: 'CUP_B_USMC_Soldier_AT',
+        B_Soldier_SL_F: 'CUP_B_USMC_Soldier_SL',
+        B_Soldier_TL_F: 'CUP_B_USMC_Soldier_TL'
+      },
+      redfor: {
+        '"OPF_F" >> "Infantry" >> "OIA_InfSquad"': '"CUP_O_RU" >> "Infantry" >> "CUP_O_RU_InfSquad_VDV_EMR"',
+        O_APC_Tracked_02_cannon_F: 'CUP_O_BTR90_RU',
+        O_Heli_Light_02_unarmed_F: 'CUP_O_Mi8_RU',
+        O_medic_F: 'CUP_O_RU_Medic',
+        O_MRAP_02_F: 'CUP_O_UAZ_MG_RU',
+        O_MRAP_02_hmg_F: 'CUP_O_GAZ_Vodnik_PK_RU',
+        O_Soldier_AR_F: 'CUP_O_RU_Soldier_AR',
+        O_Soldier_F: 'CUP_O_RU_Soldier',
+        O_Soldier_LAT_F: 'CUP_O_RU_Soldier_LAT',
+        O_Soldier_SL_F: 'CUP_O_RU_Soldier_SL',
+        O_Soldier_TL_F: 'CUP_O_RU_Soldier_TL'
+      }
+    }
+  },
+  {
+    name: 'SFP + CUP',
+    suffix: '_sfp_cup',
+    woodland: {
+      blufor: {
+        B_Heli_Transport_01_camo_F: 'CUP_B_UH60M_FFV_US',
+        B_helicrew_F: 'sfp_m90w_helipilot',
+        B_Helipilot_F: 'sfp_m90w_helipilot',
+        B_medic_F: 'sfp_m90w_medic',
+        B_Pilot_F: 'sfp_m90w_helipilot',
+        B_soldier_AAT_F: 'sfp_m90w_at_loader_grg86',
+        B_soldier_AR_F: 'sfp_m90w_automaticrifleman_ksp90',
+        B_soldier_AT_F: 'sfp_m90w_at_specialist_grg86',
+        B_Soldier_SL_F: 'sfp_m90w_squadleader',
+        B_Soldier_TL_F: 'sfp_m90w_teamleader'
+      },
+      redfor: {
+        '"OPF_F" >> "Infantry" >> "OIA_InfSquad"': '"CUP_O_RU" >> "Infantry" >> "CUP_O_RU_InfSquad_VDV_EMR"',
+        O_APC_Tracked_02_cannon_F: 'CUP_O_BTR90_RU',
+        O_Heli_Light_02_unarmed_F: 'CUP_O_Mi8_RU',
+        O_medic_F: 'CUP_O_RU_Medic',
+        O_MRAP_02_F: 'CUP_O_UAZ_MG_RU',
+        O_MRAP_02_hmg_F: 'CUP_O_GAZ_Vodnik_PK_RU',
+        O_Soldier_AR_F: 'CUP_O_RU_Soldier_AR',
+        O_Soldier_F: 'CUP_O_RU_Soldier',
+        O_Soldier_LAT_F: 'CUP_O_RU_Soldier_LAT',
+        O_Soldier_SL_F: 'CUP_O_RU_Soldier_SL',
+        O_Soldier_TL_F: 'CUP_O_RU_Soldier_TL'
+      }
+    }
+  }
+]
